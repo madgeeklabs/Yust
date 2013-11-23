250b36ffd76c01161564dbf87b7307dfd9824777
gustavog <chudijunk@gmail.com>
2013-11-23 20:26:47 +0100

    qip

--- server/routes/socketSdk.js
+++ server/routes/socketSdk.js
@@ -7,6 +7,23 @@ var availableSockets = {};
 module.exports = function (socket) {
   console.log('socket connected');
   console.log(socket.id);
-  socket.join(socket.id);
+  var gameId;
+  socket.on('createGame', function(data){
+      gameId = data.gameId;
+      socket.join(data.gameId);
+  });
+
+
+  socket.on('joinGame', function(data){
+      socket.join(data.gameId);
+  });
+    socket.on('joinGame', function(data){
+        gameId = data.gameId;
+        socket.join(gameId);
+    });
+ 
+    socket.on('control', function (data){
+        socket.broadcast.to(gameId).emit('control', data);
+    });
 
 };
