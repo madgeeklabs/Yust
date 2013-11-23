c7008319955a51ed7318917f807cb9a94c90ae2e
xavifuefer <xavifuefer@gmail.com>
2013-11-23 19:43:49 +0100

    wip

--- sdk/app/scripts/main.js
+++ sdk/app/scripts/main.js
@@ -1,21 +1,25 @@
 (function(YustSDK, $, window, document, undefined){
 
     var   serverIP = 'http://54.247.168.152:3000'
+        , widget = 'mario'
         , socket
+    ;
 
     YustSDK.init = function(){
         console.log('// Connecting...');
         socket = window.io.connect( serverIP );
 
         // switch para cada widget ?
-        socket.on('mario', function (data) {
-            console.log(data);
+        socket.on( widget, function (data) {
+            console.log('// Receiving data');
+            console.log( data );
             YustSDK.bind( data.m, data.t, data.v );
         });
     };
 
     YustSDK.bind = function( event, timestamp, value ){
-        console.log('event: ' + event);
+        console.log('// Binding event...');
+
     }
 
     return YustSDK;
