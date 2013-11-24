8411668708bb3c7e336791e8b53d109c5b7683f6
cajimhe <c.jim.her@gmail.com>
2013-11-24 09:19:51 +0100

    Improved pad?

--- client/app/scripts/fastclick.example.js
+++ client/app/scripts/fastclick.example.js
@@ -1,9 +1,9 @@
 
 // To instantiate FastClick on the body, which is the recommended method of use:
 
-window.addEventListener('load', function() {
-    FastClick.attach(document.body);
-}, false);
+// window.addEventListener('load', function() {
+//     FastClick.attach(document.body);
+// }, false);
 
 /*
 Don't forget to add a shim for addEventListener if you want to support IE8 and below.
@@ -14,3 +14,24 @@ $(function() {
     FastClick.attach(document.body);
 });
 */
+
+$(function() {
+	var buttons = document.getElementsByClassName('button'),
+		arrows = document.getElementsByClassName('arrows'),
+		arrows_d = document.getElementsByClassName('arrows');
+
+    FastClick.attach(document.body);
+
+    for (var i = 0, j = buttons.length; i < j; i++) {
+    	FastClick.attach(buttons[i]);	
+    }
+
+    for (var i = 0, j = arrows.length; i < j; i++) {
+    	FastClick.attach(arrows[i]);	
+    }
+
+    for (var i = 0, j = arrows_d.length; i < j; i++) {
+    	FastClick.attach(arrows_d[i]);	
+    }
+    
+});
